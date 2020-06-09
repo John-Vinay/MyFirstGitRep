b96defe127220fb44d6eee77a17c9cd7ad3ee754
Apple <apple@Apples-MacBook-Pro.local>
2020-06-09 11:24:44 +0530

    Maven Converted

--- programsForPractice/src/main/java/com/johnstraining/programsForPractice/ArrayFirstRepeatingOccurance.java
+++ programsForPractice/src/main/java/com/johnstraining/programsForPractice/ArrayFirstRepeatingOccurance.java
@@ -5,7 +5,7 @@ public class ArrayFirstRepeatingOccurance {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		int arr[] = { 1, 2, 3, 4, 6, 4, };
+		int arr[] = { 1, 2, 3, 4, 6, 4,5 };
 
 		for (int i = 0; i <= arr.length - 1; i++) {
 			for (int j = i + 1; j <= arr.length - 1; j++) {
